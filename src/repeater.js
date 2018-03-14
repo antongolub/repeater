4069fe9a34466c46148ee007ffb47cc39324658b
Anton Golub <mailbox@antongolub.ru>
2018-03-14 09:16:59 +0300

    docs improvements

--- src/repeater.js
+++ src/repeater.js
@@ -6,6 +6,18 @@ import type {
   ITarget
 } from './interface'
 
+/**
+ * Creates a new repeater.
+ * @param {Function} target
+ * @param {number} delay
+ * @param {*} [context]
+ * @return {IRepeater}
+ * @property {Function} target
+ * @property {number} delay
+ * @property {*} context
+ * @property {Array<*>} [args] arguments of the last invocation
+ * @property {number} [timeout] TimeoutID
+ */
 export default function createRepeater (target: ITarget, delay: number, context: ?IAny): IRepeater {
   if (!delay) {
     throw new Error('repeater: delay must not be empty')
